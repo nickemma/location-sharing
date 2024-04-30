5dd619e33eb85050aacc290802e412cf1a6b7a30
Nicholas Emmanuel <nicholasemmanuel321@gmail.com>
2024-04-30 09:20:44 +0100

    Finished the frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import {Route, Routes, Navigate } from 'react-router-dom'
 import Users from './user/pages/Users';
+import NewPlace from './places/pages/NewPlace';
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Users />} />
+      <Route path="/places/new" element={<NewPlace />} />
       <Route path="*" element={<Navigate to="/" />} />
     </Routes>
   );
--- client/src/user/pages/Users.jsx
+++ client/src/user/pages/Users.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from "react";
+
+import UsersList from "../components/UsersList";
 
 const Users = () => {
-  return (
-    <h1>
-      Hello UserList
-    </h1>
-  )
-}
+  const USERS = [
+    {
+      id: "u1",
+      name: "Techie Emma",
+      image:
+        "https://images.unsplash.com/photo-1525609004556-c46c7d6cf023?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8Y2Fyc3xlbnwwfHwwfHx8MA%3D%3D",
+      places: 3,
+    },
+  ];
+  return <UsersList items={USERS} />;
+};
 
-export default Users
+export default Users;
