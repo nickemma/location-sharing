16610bc9092b82b433a5507ac9b809b96f0d2580
Nicholas Emmanuel <nicholasemmanuel321@gmail.com>
2024-04-30 10:11:38 +0100

    Finished the frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,55 @@
-import React from 'react';
-import {Route, Routes, Navigate } from 'react-router-dom'
-import Users from './user/pages/Users';
-import NewPlace from './places/pages/NewPlace';
+import React, { useState, useCallback } from "react";
+import { Route, Routes, Navigate } from "react-router-dom";
+
+import Users from "./user/pages/Users";
+import NewPlace from "./places/pages/NewPlace";
+import UserPlaces from "./places/pages/UserPlaces";
+import UpdatePlace from "./places/pages/UpdatePlace";
+import Auth from "./user/pages/Auth";
+import MainNavigation from "./shared/components/Navigation/MainNavigation";
+import { AuthContext } from "./shared/context/auth-context";
 
 const App = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const login = useCallback(() => {
+    setIsLoggedIn(true);
+  }, []);
+
+  const logout = useCallback(() => {
+    setIsLoggedIn(false);
+  }, []);
+
+  let routes;
+  if (isLoggedIn) {
+    routes = (
+      <Routes>
+        <Route path="/" element={<Users />} />
+        <Route path="/:userId/places" element={<UserPlaces />} />
+        <Route path="/places/new" element={<NewPlace />} />
+        <Route path="/places/:placeId" element={<UpdatePlace />} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    );
+  } else {
+    routes = (
+      <Routes>
+        <Route path="/" element={<Users />} />
+        <Route path="/:userId/places" element={<UserPlaces />} />
+        <Route path="/auth" element={<Auth />} />
+        <Route path="*" element={<Navigate to="/auth" />} />
+      </Routes>
+    );
+  }
+
   return (
-    <Routes>
-      <Route path="/" element={<Users />} />
-      <Route path="/places/new" element={<NewPlace />} />
-      <Route path="*" element={<Navigate to="/" />} />
-    </Routes>
+    <AuthContext.Provider
+      value={{ isLoggedIn: isLoggedIn, login: login, logout: logout }}
+    >
+      <MainNavigation />
+      <main>{routes}</main>
+    </AuthContext.Provider>
   );
-}
+};
 
 export default App;
--- client/src/user/components/UserItem.jsx
+++ client/src/user/components/UserItem.jsx
@@ -1,22 +1,22 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-import Avatar from "../../shared/components/UI/Avatar";
-import Card from "../../shared/components/UI/Card";
+import Avatar from "../../shared/components/UIElements/Avatar";
+import Card from "../../shared/components/UIElements/Card";
 import "./UserItem.css";
 
-const UserItem = ({ id, name, image, placeCount }) => {
+const UserItem = (props) => {
   return (
     <li className="user-item">
       <Card className="user-item__content">
-        <Link to={`/${id}/places`}>
+        <Link to={`/${props.id}/places`}>
           <div className="user-item__image">
-            <Avatar image={image} alt={name} />
+            <Avatar image={props.image} alt={props.name} />
           </div>
           <div className="user-item__info">
-            <h2>{name}</h2>
+            <h2>{props.name}</h2>
             <h3>
-              {placeCount} {placeCount === 1 ? "Place" : "Places"}
+              {props.placeCount} {props.placeCount === 1 ? "Place" : "Places"}
             </h3>
           </div>
         </Link>
